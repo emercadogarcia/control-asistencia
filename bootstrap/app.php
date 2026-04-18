5f7a46c44e0d49d932e673513a64d662fa80b44b
Edgar Mercado Garcia <emercadogarcia@gmail.com>
2026-04-18 03:00:01 -0400

    se agreque codigo https para bootstrap/app.php  ::  ->booted(function () {         if (app()->environment('production')) {             URL::forceScheme('https');         }     })

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,4 +18,10 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+     ->booted(function () {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+    })
+    ->create();
